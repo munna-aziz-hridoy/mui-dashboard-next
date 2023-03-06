dc879e92f66aa089f498e8ac31118a17dc50b145
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2023-03-06 12:44:52 +0600

    comment pages for test

--- src/pages/purchase/purchase-list/index.js
+++ src/pages/purchase/purchase-list/index.js
@@ -1,167 +1,175 @@
-import React, { forwardRef, useEffect, useState } from 'react'
+// import React, { forwardRef, useEffect, useState } from 'react'
 
-//** MUI import
-import { Card, CardHeader, Divider, Grid, TextField, Button, Box } from '@mui/material'
+// //** MUI import
+// import { Card, CardHeader, Divider, Grid, TextField, Button, Box } from '@mui/material'
 
-import { AiOutlinePlus } from 'react-icons/ai'
+// import { AiOutlinePlus } from 'react-icons/ai'
 
-import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
-import DatePicker from 'react-datepicker'
+// import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+// import DatePicker from 'react-datepicker'
 
-import 'react-datepicker/dist/react-datepicker.css'
-import SelectSuplierSearch from 'src/@core/components/purchase-list/SelectSuplierSearch'
-import SelectPaymentSearch from 'src/@core/components/purchase-list/SelectPaymentSearch'
-import TableCustomized from 'src/views/tables/TableCustomized'
-import { useRouter } from 'next/router'
-import formatedDate from 'src/@core/utils/getFormatedDate'
-import { getAllInvoiceList } from 'src/@core/apiFunction/invoice'
-import { getToken } from 'src/@core/utils/manageToken'
+// import 'react-datepicker/dist/react-datepicker.css'
+// import SelectSuplierSearch from 'src/@core/components/purchase-list/SelectSuplierSearch'
+// import SelectPaymentSearch from 'src/@core/components/purchase-list/SelectPaymentSearch'
+// import TableCustomized from 'src/views/tables/TableCustomized'
+// import { useRouter } from 'next/router'
+// import formatedDate from 'src/@core/utils/getFormatedDate'
+// import { getAllInvoiceList } from 'src/@core/apiFunction/invoice'
+// import { getToken } from 'src/@core/utils/manageToken'
 
-import SelectProduct from 'src/@core/components/purchase-list/SelectProduct'
+// import SelectProduct from 'src/@core/components/purchase-list/SelectProduct'
 
-const CustomInput = forwardRef((props, ref) => {
-  return <TextField size='small' fullWidth {...props} inputRef={ref} label={props?.label} autoComplete='off' />
-})
+// const CustomInput = forwardRef((props, ref) => {
+//   return <TextField size='small' fullWidth {...props} inputRef={ref} label={props?.label} autoComplete='off' />
+// })
 
-const PurchaseList = ({}) => {
-  const [startDatePurchase, setStartDatePurchase] = useState(new Date())
-  const [endDatePurchase, setEndDatePurchase] = useState()
-  const [startDateCreated, setStartDateCreated] = useState(new Date())
-  const [endDateCreated, setEndDateCreated] = useState()
+// const PurchaseList = ({}) => {
+//   const [startDatePurchase, setStartDatePurchase] = useState(new Date())
+//   const [endDatePurchase, setEndDatePurchase] = useState()
+//   const [startDateCreated, setStartDateCreated] = useState(new Date())
+//   const [endDateCreated, setEndDateCreated] = useState()
 
-  const [supplier, setSupplier] = useState('')
-  const [paymentStatus, setPaymentStatus] = useState('')
-  const [productName, setProductName] = useState('')
+//   const [supplier, setSupplier] = useState('')
+//   const [paymentStatus, setPaymentStatus] = useState('')
+//   const [productName, setProductName] = useState('')
 
-  const [invoices, setInvoices] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [refetch, setRefetch] = useState(false)
-  const [clearForm, setClearForm] = useState(false)
+//   const [invoices, setInvoices] = useState([])
+//   const [loading, setLoading] = useState(false)
+//   const [refetch, setRefetch] = useState(false)
+//   const [clearForm, setClearForm] = useState(false)
 
-  const router = useRouter()
+//   const router = useRouter()
 
-  const { access_token } = getToken()
+//   const { access_token } = getToken()
 
-  useEffect(() => {
-    setLoading(true)
+//   useEffect(() => {
+//     setLoading(true)
 
-    const formatedStartDatePurchase = startDatePurchase ? formatedDate(startDatePurchase) : ''
-    const formatedEndDatePurchase = endDatePurchase ? formatedDate(endDatePurchase) : ''
+//     const formatedStartDatePurchase = startDatePurchase ? formatedDate(startDatePurchase) : ''
+//     const formatedEndDatePurchase = endDatePurchase ? formatedDate(endDatePurchase) : ''
 
-    const formatedStartDateCreated = startDateCreated ? formatedDate(startDateCreated) : ''
-    const formatedEndDateCreated = endDateCreated ? formatedDate(endDateCreated) : ''
+//     const formatedStartDateCreated = startDateCreated ? formatedDate(startDateCreated) : ''
+//     const formatedEndDateCreated = endDateCreated ? formatedDate(endDateCreated) : ''
 
-    async function fetchData() {
-      await getAllInvoiceList(
-        productName,
-        [formatedStartDatePurchase, formatedEndDatePurchase],
-        [formatedStartDateCreated, formatedEndDateCreated],
-        supplier,
-        paymentStatus,
-        access_token
-      ).then(data => {
-        setInvoices(data)
-        setLoading(false)
-      })
-    }
+//     async function fetchData() {
+//       await getAllInvoiceList(
+//         productName,
+//         [formatedStartDatePurchase, formatedEndDatePurchase],
+//         [formatedStartDateCreated, formatedEndDateCreated],
+//         supplier,
+//         paymentStatus,
+//         access_token
+//       ).then(data => {
+//         setInvoices(data)
+//         setLoading(false)
+//       })
+//     }
 
-    fetchData()
-  }, [refetch])
+//     fetchData()
+//   }, [refetch])
 
-  const handleSearch = () => {
-    setRefetch(prev => !prev)
-    setClearForm(prev => !prev)
-  }
+//   const handleSearch = () => {
+//     setRefetch(prev => !prev)
+//     setClearForm(prev => !prev)
+//   }
 
-  const handleResetFilter = () => {
-    setProductName('')
-    setEndDatePurchase('')
-    setStartDatePurchase('')
-    setEndDateCreated('')
-    setStartDateCreated('')
-    setSupplier('')
-    setPaymentStatus('')
-    setRefetch(prev => !prev)
-    setClearForm(prev => !prev)
-  }
+//   const handleResetFilter = () => {
+//     setProductName('')
+//     setEndDatePurchase('')
+//     setStartDatePurchase('')
+//     setEndDateCreated('')
+//     setStartDateCreated('')
+//     setSupplier('')
+//     setPaymentStatus('')
+//     setRefetch(prev => !prev)
+//     setClearForm(prev => !prev)
+//   }
 
-  return (
-    <>
-      <Card style={{ padding: '1px 20px', overflow: 'visible', marginBottom: '30px' }}>
-        <CardHeader style={{ padding: '10px 25px' }} title='Purchase List' titleTypographyProps={{ variant: 'h6' }} />
-        <Divider sx={{ margin: 0 }} />
+//   return (
+//     <>
+//       <Card style={{ padding: '1px 20px', overflow: 'visible', marginBottom: '30px' }}>
+//         <CardHeader style={{ padding: '10px 25px' }} title='Purchase List' titleTypographyProps={{ variant: 'h6' }} />
+//         <Divider sx={{ margin: 0 }} />
 
-        <Grid container spacing={2.4} marginTop={1} marginBottom={3} justifyContent='center'>
-          <Grid item height={50} xs={2}>
-            <SelectProduct selectedProduct={productName} setSelectedProduct={setProductName} clearForm={clearForm} />
-          </Grid>
-          <Grid item xs={2.4}>
-            <DatePickerWrapper>
-              <DatePicker
-                selectsRange={true}
-                showYearDropdown
-                showMonthDropdown
-                placeholderText='MM-DD-YYYY'
-                customInput={<CustomInput label='Purchase Date' />}
-                onChange={value => {
-                  setStartDatePurchase(value[0])
-                  setEndDatePurchase(value[1])
-                }}
-                startDate={startDatePurchase}
-                endDate={endDatePurchase}
-              />
-            </DatePickerWrapper>
-          </Grid>
-          <Grid item xs={2.4}>
-            <DatePickerWrapper>
-              <DatePicker
-                selectsRange={true}
-                showYearDropdown
-                showMonthDropdown
-                placeholderText='MM-DD-YYYY'
-                customInput={<CustomInput label='Created Date' />}
-                onChange={value => {
-                  setStartDateCreated(value[0])
-                  setEndDateCreated(value[1])
-                }}
-                startDate={startDateCreated}
-                endDate={endDateCreated}
-              />
-            </DatePickerWrapper>
-          </Grid>
+//         <Grid container spacing={2.4} marginTop={1} marginBottom={3} justifyContent='center'>
+//           <Grid item height={50} xs={2}>
+//             <SelectProduct selectedProduct={productName} setSelectedProduct={setProductName} clearForm={clearForm} />
+//           </Grid>
+//           <Grid item xs={2.4}>
+//             <DatePickerWrapper>
+//               <DatePicker
+//                 selectsRange={true}
+//                 showYearDropdown
+//                 showMonthDropdown
+//                 placeholderText='MM-DD-YYYY'
+//                 customInput={<CustomInput label='Purchase Date' />}
+//                 onChange={value => {
+//                   setStartDatePurchase(value[0])
+//                   setEndDatePurchase(value[1])
+//                 }}
+//                 startDate={startDatePurchase}
+//                 endDate={endDatePurchase}
+//               />
+//             </DatePickerWrapper>
+//           </Grid>
+//           <Grid item xs={2.4}>
+//             <DatePickerWrapper>
+//               <DatePicker
+//                 selectsRange={true}
+//                 showYearDropdown
+//                 showMonthDropdown
+//                 placeholderText='MM-DD-YYYY'
+//                 customInput={<CustomInput label='Created Date' />}
+//                 onChange={value => {
+//                   setStartDateCreated(value[0])
+//                   setEndDateCreated(value[1])
+//                 }}
+//                 startDate={startDateCreated}
+//                 endDate={endDateCreated}
+//               />
+//             </DatePickerWrapper>
+//           </Grid>
 
-          <SelectSuplierSearch setSupplier={setSupplier} />
+//           <SelectSuplierSearch setSupplier={setSupplier} />
 
-          <SelectPaymentSearch setPayment={setPaymentStatus} />
+//           <SelectPaymentSearch setPayment={setPaymentStatus} />
 
-          <Grid item xs={12}>
-            <Box component='div' display='flex' gap={5}>
-              <Button onClick={handleSearch} fullWidth variant='contained' size='small'>
-                Search
-              </Button>
-              <Button onClick={handleResetFilter} fullWidth variant='outlined' size='small'>
-                Reset Filter
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </Card>
+//           <Grid item xs={12}>
+//             <Box component='div' display='flex' gap={5}>
+//               <Button onClick={handleSearch} fullWidth variant='contained' size='small'>
+//                 Search
+//               </Button>
+//               <Button onClick={handleResetFilter} fullWidth variant='outlined' size='small'>
+//                 Reset Filter
+//               </Button>
+//             </Box>
+//           </Grid>
+//         </Grid>
+//       </Card>
 
-      <Button
-        variant='contained'
-        size='small'
-        style={{ display: 'inline-block', marginBottom: '20px' }}
-        onClick={() => router.push('/purchase/add-purchase')}
-      >
-        <AiOutlinePlus fontSize={16} color='#fff' style={{ marginRight: '5px' }} />
-        Add Purchase
-      </Button>
+//       <Button
+//         variant='contained'
+//         size='small'
+//         style={{ display: 'inline-block', marginBottom: '20px' }}
+//         onClick={() => router.push('/purchase/add-purchase')}
+//       >
+//         <AiOutlinePlus fontSize={16} color='#fff' style={{ marginRight: '5px' }} />
+//         Add Purchase
+//       </Button>
 
-      <Card>
-        <TableCustomized setRefetch={setRefetch} invoices={invoices} loading={loading} refetch={refetch} />
-      </Card>
-    </>
-  )
+//       <Card>
+//         <TableCustomized setRefetch={setRefetch} invoices={invoices} loading={loading} refetch={refetch} />
+//       </Card>
+//     </>
+//   )
+// }
+
+// export default PurchaseList
+
+import React from 'react'
+
+const Index = () => {
+  return <div>Index</div>
 }
 
-export default PurchaseList
+export default Index
--- src/pages/purchase/purchase-overview/index.js
+++ src/pages/purchase/purchase-overview/index.js
@@ -1,208 +1,216 @@
-import React, { forwardRef, useEffect, useState } from 'react'
-import { Box, Button, Card, CardHeader, Divider, Grid, Tab as MuiTab, TextField, Typography } from '@mui/material'
+// import React, { forwardRef, useEffect, useState } from 'react'
+// import { Box, Button, Card, CardHeader, Divider, Grid, Tab as MuiTab, TextField, Typography } from '@mui/material'
 
-import { AiFillEye } from 'react-icons/ai'
+// import { AiFillEye } from 'react-icons/ai'
 
-import DatePicker from 'react-datepicker'
-import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
-import { TabContext, TabList, TabPanel } from '@mui/lab'
-import { styled } from '@mui/material/styles'
-import ItemTable from 'src/@core/components/purchase-overview/ItemTable'
-import SupplierTable from 'src/@core/components/purchase-overview/supplierTable'
-import ViewInvoiceModal from 'src/@core/components/modal/viewInvoiceListModal'
+// import DatePicker from 'react-datepicker'
+// import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+// import { TabContext, TabList, TabPanel } from '@mui/lab'
+// import { styled } from '@mui/material/styles'
+// import ItemTable from 'src/@core/components/purchase-overview/ItemTable'
+// import SupplierTable from 'src/@core/components/purchase-overview/supplierTable'
+// import ViewInvoiceModal from 'src/@core/components/modal/viewInvoiceListModal'
 
-import 'react-datepicker/dist/react-datepicker.css'
-import { purchaseOverview } from 'src/@core/apiFunction/invoice'
-import formatedDate from 'src/@core/utils/getFormatedDate'
-import { getToken } from 'src/@core/utils/manageToken'
+// import 'react-datepicker/dist/react-datepicker.css'
+// import { purchaseOverview } from 'src/@core/apiFunction/invoice'
+// import formatedDate from 'src/@core/utils/getFormatedDate'
+// import { getToken } from 'src/@core/utils/manageToken'
 
-const CustomInput = forwardRef((props, ref) => {
-  return <TextField size='small' fullWidth {...props} inputRef={ref} label={props.label} autoComplete='off' />
-})
+// const CustomInput = forwardRef((props, ref) => {
+//   return <TextField size='small' fullWidth {...props} inputRef={ref} label={props.label} autoComplete='off' />
+// })
 
-const StyledTypography = ({ children, label, icon = null }) => {
-  return (
-    <Typography variant='body2' display='flex' alignItems='center' gap={2}>
-      {label} : <span style={{ fontWeight: '600' }}>{children}</span> {icon && icon}
-    </Typography>
-  )
+// const StyledTypography = ({ children, label, icon = null }) => {
+//   return (
+//     <Typography variant='body2' display='flex' alignItems='center' gap={2}>
+//       {label} : <span style={{ fontWeight: '600' }}>{children}</span> {icon && icon}
+//     </Typography>
+//   )
+// }
+
+// const Tab = styled(MuiTab)(({ theme }) => ({
+//   [theme.breakpoints.down('md')]: {
+//     minWidth: 100
+//   },
+//   [theme.breakpoints.down('sm')]: {
+//     minWidth: 67
+//   }
+// }))
+
+// const TabName = styled('span')(({ theme }) => ({
+//   lineHeight: 1.71,
+//   fontSize: '0.875rem',
+//   marginLeft: theme.spacing(2.4),
+//   [theme.breakpoints.down('md')]: {
+//     display: 'none'
+//   }
+// }))
+
+// function last30Days() {
+//   const result = []
+//   for (let i = 0; i < 30; i++) {
+//     let d = new Date()
+//     d.setDate(d.getDate() - i)
+//     result.push(d)
+//   }
+
+//   return result
+// }
+
+// const PurchaseOverview = () => {
+//   const [purchaseOverviewData, setPurchaseOverviewData] = useState(null)
+//   const [loading, setLoading] = useState(false)
+
+//   const [createdStartDate, setCreatedStartDate] = useState(last30Days()[29])
+//   const [createdEndDate, setCreatedEndDate] = useState(new Date())
+
+//   const [invoiceStartDate, setInvoiceStartDate] = useState()
+//   const [invoiceEndDate, setInvoiceEndDate] = useState()
+
+//   const [tabValue, setTabValue] = useState('item')
+
+//   const [openInvoiceListModal, setOpenInvoiceListModal] = useState(false)
+
+//   const [refetch, setRefetch] = useState(false)
+
+//   const [pageProduct, setPageProduct] = useState(1)
+//   const [pageSup, setPageSup] = useState(1)
+
+//   const { access_token } = getToken()
+
+//   useEffect(() => {
+//     const formatedCreatedDate =
+//       createdEndDate && createdStartDate ? [formatedDate(createdStartDate), formatedDate(createdEndDate)] : []
+
+//     const formatedInvoiceDate =
+//       invoiceStartDate && invoiceEndDate ? [formatedDate(invoiceStartDate), formatedDate(invoiceEndDate)] : []
+
+//     setLoading(true)
+//     purchaseOverview(formatedCreatedDate, formatedInvoiceDate, access_token, pageProduct, pageSup).then(data => {
+//       if (data?.invoices) {
+//         setPurchaseOverviewData(data)
+//       }
+//     })
+//   }, [refetch, pageProduct, pageSup])
+
+//   const handleTabChange = (event, newValue) => {
+//     setTabValue(newValue)
+//   }
+
+//   const handleRefetch = () => {
+//     setRefetch(prev => !prev)
+//   }
+
+//   return (
+//     <Card>
+//       <CardHeader title='Purchase Overview' />
+//       <Divider />
+//       <Grid container spacing={3} padding={5}>
+//         <Grid item xs={12} sm={4}>
+//           <DatePickerWrapper>
+//             <DatePicker
+//               selectsRange={true}
+//               showYearDropdown
+//               showMonthDropdown
+//               placeholderText='MM-DD-YYYY'
+//               customInput={<CustomInput label='Created Date' />}
+//               onChange={value => {
+//                 setCreatedStartDate(value[0])
+//                 setCreatedEndDate(value[1])
+//               }}
+//               startDate={createdStartDate}
+//               endDate={createdEndDate}
+//             />
+//           </DatePickerWrapper>
+//         </Grid>
+//         <Grid item xs={12} sm={4}>
+//           <DatePickerWrapper>
+//             <DatePicker
+//               selectsRange={true}
+//               showYearDropdown
+//               showMonthDropdown
+//               placeholderText='MM-DD-YYYY'
+//               customInput={<CustomInput label='Invoice Date' />}
+//               onChange={value => {
+//                 setInvoiceStartDate(value[0])
+//                 setInvoiceEndDate(value[1])
+//               }}
+//               startDate={invoiceStartDate}
+//               endDate={invoiceEndDate}
+//             />
+//           </DatePickerWrapper>
+//         </Grid>
+
+//         <Grid item xs={12} sm={4}>
+//           <Button onClick={handleRefetch} variant='contained' fullWidth>
+//             Get Overview
+//           </Button>
+//         </Grid>
+//       </Grid>
+
+//       <Divider />
+
+//       <Box component='div' padding={5}>
+//         <StyledTypography
+//           label='Total Purchases'
+//           icon={
+//             <AiFillEye
+//               fontSize={20}
+//               style={{ marginTop: '-2px', cursor: 'pointer' }}
+//               onClick={() => setOpenInvoiceListModal(true)}
+//             />
+//           }
+//         >
+//           {purchaseOverviewData?.invoices?.invoice_count}
+//         </StyledTypography>
+//         <StyledTypography label='Purchase amount'>
+//           ¥{purchaseOverviewData?.invoices?.total_purchase_amount}
+//         </StyledTypography>
+//         <StyledTypography label='Grand Total'>
+//           ¥{purchaseOverviewData?.invoices?.total_invoice_amount?.toFixed(2)}
+//         </StyledTypography>
+//         <StyledTypography label='Total item'>{purchaseOverviewData?.items?.total_records}</StyledTypography>
+//         <Divider style={{ width: '40%' }} />
+
+//         <StyledTypography label='Paid amount'>
+//           ¥${purchaseOverviewData?.invoices?.total_paid_amount?.toFixed(2)}
+//         </StyledTypography>
+//         <StyledTypography label='Partial pay amount'>
+//           ¥{purchaseOverviewData?.invoices?.total_partial_paid_amount}
+//         </StyledTypography>
+//         <StyledTypography label='Total Due'>
+//           ¥{purchaseOverviewData?.invoices?.total_unpaid_amount?.toFixed(2)}
+//         </StyledTypography>
+//         <StyledTypography label='Unpaid'>{purchaseOverviewData?.invoices?.total_unpaid_count}</StyledTypography>
+
+//         <ViewInvoiceModal open={openInvoiceListModal} setOpen={setOpenInvoiceListModal} />
+//       </Box>
+
+//       <Box component='div'>
+//         <TabContext value={tabValue}>
+//           <TabList onChange={handleTabChange} sx={{ borderBottom: theme => `1px solid ${theme.palette.divider}` }}>
+//             <Tab value='item' label={<TabName>All Items</TabName>} />
+//             <Tab value='supplier' label={<TabName>Supplier</TabName>} />
+//           </TabList>
+
+//           <TabPanel value='item'>
+//             <ItemTable data={purchaseOverviewData?.items} pageCount={setPageProduct} />
+//           </TabPanel>
+//           <TabPanel value='supplier'>
+//             <SupplierTable data={purchaseOverviewData?.suppliers} pageCount={setPageSup} />
+//           </TabPanel>
+//         </TabContext>
+//       </Box>
+//     </Card>
+//   )
+// }
+
+// export default PurchaseOverview
+
+import React from 'react'
+
+const Index = () => {
+  return <div>Index</div>
 }
 
-const Tab = styled(MuiTab)(({ theme }) => ({
-  [theme.breakpoints.down('md')]: {
-    minWidth: 100
-  },
-  [theme.breakpoints.down('sm')]: {
-    minWidth: 67
-  }
-}))
-
-const TabName = styled('span')(({ theme }) => ({
-  lineHeight: 1.71,
-  fontSize: '0.875rem',
-  marginLeft: theme.spacing(2.4),
-  [theme.breakpoints.down('md')]: {
-    display: 'none'
-  }
-}))
-
-function last30Days() {
-  const result = []
-  for (let i = 0; i < 30; i++) {
-    let d = new Date()
-    d.setDate(d.getDate() - i)
-    result.push(d)
-  }
-
-  return result
-}
-
-const PurchaseOverview = () => {
-  const [purchaseOverviewData, setPurchaseOverviewData] = useState(null)
-  const [loading, setLoading] = useState(false)
-
-  const [createdStartDate, setCreatedStartDate] = useState(last30Days()[29])
-  const [createdEndDate, setCreatedEndDate] = useState(new Date())
-
-  const [invoiceStartDate, setInvoiceStartDate] = useState()
-  const [invoiceEndDate, setInvoiceEndDate] = useState()
-
-  const [tabValue, setTabValue] = useState('item')
-
-  const [openInvoiceListModal, setOpenInvoiceListModal] = useState(false)
-
-  const [refetch, setRefetch] = useState(false)
-
-  const [pageProduct, setPageProduct] = useState(1)
-  const [pageSup, setPageSup] = useState(1)
-
-  const { access_token } = getToken()
-
-  useEffect(() => {
-    const formatedCreatedDate =
-      createdEndDate && createdStartDate ? [formatedDate(createdStartDate), formatedDate(createdEndDate)] : []
-
-    const formatedInvoiceDate =
-      invoiceStartDate && invoiceEndDate ? [formatedDate(invoiceStartDate), formatedDate(invoiceEndDate)] : []
-
-    setLoading(true)
-    purchaseOverview(formatedCreatedDate, formatedInvoiceDate, access_token, pageProduct, pageSup).then(data => {
-      if (data?.invoices) {
-        setPurchaseOverviewData(data)
-      }
-    })
-  }, [refetch, pageProduct, pageSup])
-
-  const handleTabChange = (event, newValue) => {
-    setTabValue(newValue)
-  }
-
-  const handleRefetch = () => {
-    setRefetch(prev => !prev)
-  }
-
-  return (
-    <Card>
-      <CardHeader title='Purchase Overview' />
-      <Divider />
-      <Grid container spacing={3} padding={5}>
-        <Grid item xs={12} sm={4}>
-          <DatePickerWrapper>
-            <DatePicker
-              selectsRange={true}
-              showYearDropdown
-              showMonthDropdown
-              placeholderText='MM-DD-YYYY'
-              customInput={<CustomInput label='Created Date' />}
-              onChange={value => {
-                setCreatedStartDate(value[0])
-                setCreatedEndDate(value[1])
-              }}
-              startDate={createdStartDate}
-              endDate={createdEndDate}
-            />
-          </DatePickerWrapper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <DatePickerWrapper>
-            <DatePicker
-              selectsRange={true}
-              showYearDropdown
-              showMonthDropdown
-              placeholderText='MM-DD-YYYY'
-              customInput={<CustomInput label='Invoice Date' />}
-              onChange={value => {
-                setInvoiceStartDate(value[0])
-                setInvoiceEndDate(value[1])
-              }}
-              startDate={invoiceStartDate}
-              endDate={invoiceEndDate}
-            />
-          </DatePickerWrapper>
-        </Grid>
-
-        <Grid item xs={12} sm={4}>
-          <Button onClick={handleRefetch} variant='contained' fullWidth>
-            Get Overview
-          </Button>
-        </Grid>
-      </Grid>
-
-      <Divider />
-
-      <Box component='div' padding={5}>
-        <StyledTypography
-          label='Total Purchases'
-          icon={
-            <AiFillEye
-              fontSize={20}
-              style={{ marginTop: '-2px', cursor: 'pointer' }}
-              onClick={() => setOpenInvoiceListModal(true)}
-            />
-          }
-        >
-          {purchaseOverviewData?.invoices?.invoice_count}
-        </StyledTypography>
-        <StyledTypography label='Purchase amount'>
-          ¥{purchaseOverviewData?.invoices?.total_purchase_amount}
-        </StyledTypography>
-        <StyledTypography label='Grand Total'>
-          ¥{purchaseOverviewData?.invoices?.total_invoice_amount?.toFixed(2)}
-        </StyledTypography>
-        <StyledTypography label='Total item'>{purchaseOverviewData?.items?.total_records}</StyledTypography>
-        <Divider style={{ width: '40%' }} />
-
-        <StyledTypography label='Paid amount'>
-          ¥${purchaseOverviewData?.invoices?.total_paid_amount?.toFixed(2)}
-        </StyledTypography>
-        <StyledTypography label='Partial pay amount'>
-          ¥{purchaseOverviewData?.invoices?.total_partial_paid_amount}
-        </StyledTypography>
-        <StyledTypography label='Total Due'>
-          ¥{purchaseOverviewData?.invoices?.total_unpaid_amount?.toFixed(2)}
-        </StyledTypography>
-        <StyledTypography label='Unpaid'>{purchaseOverviewData?.invoices?.total_unpaid_count}</StyledTypography>
-
-        <ViewInvoiceModal open={openInvoiceListModal} setOpen={setOpenInvoiceListModal} />
-      </Box>
-
-      <Box component='div'>
-        <TabContext value={tabValue}>
-          <TabList onChange={handleTabChange} sx={{ borderBottom: theme => `1px solid ${theme.palette.divider}` }}>
-            <Tab value='item' label={<TabName>All Items</TabName>} />
-            <Tab value='supplier' label={<TabName>Supplier</TabName>} />
-          </TabList>
-
-          <TabPanel value='item'>
-            <ItemTable data={purchaseOverviewData?.items} pageCount={setPageProduct} />
-          </TabPanel>
-          <TabPanel value='supplier'>
-            <SupplierTable data={purchaseOverviewData?.suppliers} pageCount={setPageSup} />
-          </TabPanel>
-        </TabContext>
-      </Box>
-    </Card>
-  )
-}
-
-export default PurchaseOverview
+export default Index
